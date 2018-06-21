22a93c4b3a08d1153772f33c4a43a502c8bc5710
Martin_Yan <Maitong_y@163.com>
2018-06-21 20:50:44 +0800

    update readme

--- BabyMother/findlatestweight.py
+++ BabyMother/findlatestweight.py
@@ -39,19 +39,22 @@ data[(data['BMI']>22) & (data['年龄']<36)]
 data.to_excel('/Users/martin_yan/Desktop/1.xlsx', index=False, encoding="utf_8_sig",header=True)
 """
 
-data=pd.read_csv('../data/宝妈用户每日体重变化5.22-6.11.csv')
-#data=data[data['日期']> '2018/5/28 0:00']
+data=pd.read_csv('../data/宝妈用户每日体重变化5.22-6.18.csv')
+data2=pd.read_csv('../data/宝妈用户初始信息表.csv',usecols=['姓名','BMI'])
+data = pd.merge(data, data2, on='姓名')
+data = data.drop_duplicates(['uid','日期'],keep='last')
+data=data.reset_index(drop=True)
+#data=data[data['日期']>'2018/5/28 0:00']
 #data=data[data['日期']< '2018/6/5 0:00']
-#data=data[(True-data['日期'].isin(['2018/6/10 0:00','2018/6/11 0:00']))]
-#data=data[(True-data['日期'].isin(['2018/6/2 0:00','2018/6/3 0:00','2018/6/5 0:00']))]
-#print(data)
+#data=data[(True-data['日期'].isin(['2018/6/10 0:00','2018/6/11 0:00','2018/6/12 0:00','2018/6/13 0:00','2018/6/14 0:00','2018/6/15 0:00','2018/6/16 0:00','2018/6/17 0:00','2018/6/18 0:00']))]
+data=data[(True-data['日期'].isin(['2018/6/12 0:00','2018/6/13 0:00','2018/6/14 0:00','2018/6/15 0:00','2018/6/16 0:00','2018/6/17 0:00','2018/6/18 0:00']))]
 fre=data.groupby('uid').count()['姓名'].to_dict()
 a,b = list(fre.keys()) , list(fre.values())
 fre = pd.DataFrame({'uid':a,'实际记录天数':b})
 #fre_series=data['用户编号'].value_counts()
 #fre = fre_series.to_frame('实际记录天数')
 data = pd.merge(fre, data, how='left', on='uid')
-data=data[data['实际记录天数']>14]
+data=data[data['实际记录天数']>17]
 print(len(data['uid'].unique()))
 data=data.reset_index(drop=True)
 first=data.duplicated(['姓名'],keep='first')
@@ -60,6 +63,7 @@ reduce=[]
 id=[]
 name=[]
 day=[]
+BMI=[]
 for i in range(len(data)):
     if first[i]==False:
         original=data.iloc[i]['体重']
@@ -68,7 +72,11 @@ for i in range(len(data)):
         name.append(data.iloc[i]['姓名'])
         day.append(data.iloc[i]['实际记录天数'])
         reduce.append(original-data.iloc[i]['体重'])
-dataframe=pd.DataFrame({'用户编号':id,'姓名':name,'实际记录天数':day,'减重值':reduce})
-columns=['用户编号','姓名','实际记录天数','减重值']
+        if data.iloc[i]['BMI']>24:
+            BMI.append('A组(BMI>24)')
+        else:
+            BMI.append('B组(BMI<=24)')
+dataframe=pd.DataFrame({'用户编号':id,'姓名':name,'实际记录天数':day,'减重值':reduce,'BMI':BMI})
+columns=['用户编号','姓名','实际记录天数','减重值','BMI']
 print(dataframe)
-dataframe.to_csv('/Users/martin_yan/Desktop/第三周累积减重值情况.csv', index=False, encoding="utf_8_sig",columns=columns)
+dataframe.to_csv('/Users/martin_yan/Desktop/累积第三周减重值情况.csv', index=False, encoding="utf_8_sig",columns=columns)
