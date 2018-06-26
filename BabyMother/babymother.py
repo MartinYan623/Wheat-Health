07fd3fa8ad8969781e8e0ff3e0f7815d5bda84a9
Martin_Yan <Maitong_y@163.com>
2018-06-26 14:03:54 +0800

    modify the layout and content of output

--- BabyMother/babymother.py
+++ BabyMother/babymother.py
@@ -10,11 +10,14 @@ data=data.append(data4)
 #用uid对数据排序，默认升序
 #data=data.sort_values(['uid','记录日期'])
 data=data[(True-data['rf'].isin(['有氧运动','吸烟']))]
-print('宝妈营总人数：'+str(len(data['uid'].unique())))
-data = data.drop_duplicates(['uid','记录日期', 'rf'])
+#data = data.drop_duplicates(['uid','记录日期', 'rf'])
+data=data[data['uid']>44687]
 data=data.reset_index(drop=True)
 date=data.duplicated(['uid','记录日期'],keep='last')
-print(data)
+first=data.duplicated(['uid','记录日期','rf'],keep='first')
+last=data.duplicated(['uid','记录日期','rf'],keep='last')
+print('宝妈营总人数：'+str(len(data['uid'].unique())))
+
 userid = []
 name = []
 f_fruit = []
@@ -41,9 +44,12 @@ f_salt = []
 salt = []
 f_sugar = []
 sugar = []
-f_totalheat = []
+f_totalheat1 = []
+f_totalheat2 = []
 totalheat = []
-f_nutrients = []
+f_nutrients1 = []
+f_nutrients2 = []
+f_nutrients3 = []
 nutrients = []
 f_alcohol = []
 alcohol = []
@@ -52,70 +58,93 @@ water = []
 time=[]
 totalscore=[]
 row=0
+countf=0
 for i in range(len(data)):
     if data.iloc[i]['rf'] == '水果摄入量':
-        fruit.append(data.iloc[i]['rf得分'])
-        f_fruit.append(data.iloc[i]['摄入值'])
+        if first[i]==False:
+            fruit.append(data.iloc[i]['rf得分'])
+            f_fruit.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '蔬菜摄入量':
-        veg.append(data.iloc[i]['rf得分'])
-        f_veg.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            veg.append(data.iloc[i]['rf得分'])
+            f_veg.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '全谷类摄入量':
-        wholegrain.append(data.iloc[i]['rf得分'])
-        f_wholegrain.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            wholegrain.append(data.iloc[i]['rf得分'])
+            f_wholegrain.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '精制谷物摄入量':
-        refinegrain.append(data.iloc[i]['rf得分'])
-        f_refinegrain.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            refinegrain.append(data.iloc[i]['rf得分'])
+            f_refinegrain.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '膳食纤维摄入量':
-        diefiber.append(data.iloc[i]['rf得分'])
-        f_diefiber.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            diefiber.append(data.iloc[i]['rf得分'])
+            f_diefiber.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '乳类摄入量':
-        milk.append(data.iloc[i]['rf得分'])
-        f_milk.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            milk.append(data.iloc[i]['rf得分'])
+            f_milk.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '总蛋白摄入量':
-        totalprotein.append(data.iloc[i]['rf得分'])
-        f_totalprotein.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            totalprotein.append(data.iloc[i]['rf得分'])
+            f_totalprotein.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '鱼虾贝壳类及植物蛋白类摄入量':
-        fishshrimp.append(data.iloc[i]['rf得分'])
-        f_fishshrimp.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            fishshrimp.append(data.iloc[i]['rf得分'])
+            f_fishshrimp.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '不饱和与饱和脂肪酸摄入比':
-        fattyacids.append(data.iloc[i]['rf得分'])
-        f_fattyacids.append(data.iloc[i]['摄入值'])
+        if first[i]==False:
+            fattyacids.append(data.iloc[i]['rf得分'])
+            f_fattyacids.append(data.iloc[i]['对比值'])
 
     if data.iloc[i]['rf'] == '固态脂肪摄入量':
-        solidfat.append(data.iloc[i]['rf得分'])
-        f_solidfat.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            solidfat.append(data.iloc[i]['rf得分'])
+            f_solidfat.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '钠盐摄入量':
-        salt.append(data.iloc[i]['rf得分'])
-        f_salt.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            salt.append(data.iloc[i]['rf得分'])
+            f_salt.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '添加糖摄入量':
-        sugar.append(data.iloc[i]['rf得分'])
-        f_sugar.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            sugar.append(data.iloc[i]['rf得分'])
+            f_sugar.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '总热量摄入量':
-        totalheat.append(data.iloc[i]['rf得分'])
-        f_totalheat.append(data.iloc[i]['对比值'])
+        if first[i] == False:
+            totalheat.append(data.iloc[i]['rf得分'])
+            f_totalheat1.append(data.iloc[i]['摄入值'])
+            f_totalheat2.append(data.iloc[i]['对比值'])
 
     if data.iloc[i]['rf'] == '三大营养素组成':
-        nutrients.append(data.iloc[i]['rf得分'])
-        f_nutrients.append(data.iloc[i]['摄入值'])
+        if first[i]==False and last[i]==True:
+            f_nutrients1.append(data.iloc[i]['对比值'])
+        if first[i] == True and last[i] == True and countf==0:
+            f_nutrients2.append(data.iloc[i]['对比值'])
+            countf=countf+1
+        if first[i]==True and last[i]==False:
+            nutrients.append(data.iloc[i]['rf得分'])
+            f_nutrients3.append(data.iloc[i]['对比值'])
 
     if data.iloc[i]['rf'] == '饮酒（酒精量，全天标准）':
-        alcohol.append(data.iloc[i]['rf得分'])
-        f_alcohol.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            alcohol.append(data.iloc[i]['rf得分'])
+            f_alcohol.append(data.iloc[i]['摄入值'])
 
     if data.iloc[i]['rf'] == '饮水量':
-        water.append(data.iloc[i]['rf得分'])
-        f_water.append(data.iloc[i]['摄入值'])
+        if first[i] == False:
+            water.append(data.iloc[i]['rf得分'])
+            f_water.append(data.iloc[i]['摄入值'])
 
     if date[i]==False:
         time.append(data.iloc[i]['记录日期'])
@@ -124,9 +153,29 @@ for i in range(len(data)):
         totalscore.append(fruit[row]+veg[row]+wholegrain[row]+refinegrain[row]+diefiber[row]+milk[row]+totalprotein[row]+fishshrimp[row]+fattyacids[row]
                       +solidfat[row]+salt[row]+sugar[row]+totalheat[row]+nutrients[row]+alcohol[row]+water[row])
         row=row+1
+        countf=0
 
+print(len(userid))
+print(len(f_fruit))
+print(len(f_veg))
+print(len(f_wholegrain))
+print(len(f_refinegrain))
+print(len(f_diefiber))
 
+print(len(f_milk))
+print(len( f_fishshrimp))
+print(len(f_fattyacids))
 
+print(len(alcohol))
+print(len(f_nutrients1))
+print(len(f_nutrients2))
+print(len(f_nutrients3))
+print(len(solidfat))
+print(len(f_salt))
+print(len(f_totalprotein))
+print(len(f_diefiber))
+
+print(len(totalscore))
 
 dataframe = pd.DataFrame({'用户编号': userid, '姓名': name, '记录日期': time,
                               '水果摄入实际量': f_fruit, '水果摄入量得分': fruit,
@@ -137,22 +186,24 @@ dataframe = pd.DataFrame({'用户编号': userid, '姓名': name, '记录日期'
                               '乳类摄入实际量': f_milk, '乳类摄入量得分': milk,
                               '总蛋白摄入实际量': f_totalprotein, '总蛋白摄入量得分': totalprotein,
                               '鱼虾贝壳类及植物蛋白类摄入实际量': f_fishshrimp, '鱼虾贝壳类及植物蛋白类摄入量得分': fishshrimp,
-                              '不饱和与饱和脂肪酸实际摄入量': f_fattyacids, '不饱和与饱和脂肪酸摄入比得分': fattyacids,
+                              '不饱和与饱和脂肪酸摄入对比值': f_fattyacids, '不饱和与饱和脂肪酸摄入比得分': fattyacids,
                               '固态脂肪摄入实际量': f_solidfat, '固态脂肪摄入量得分': solidfat,
                               '钠盐摄入实际量': f_salt, '钠盐摄入量得分': salt,
-                              '添加糖摄入实际量': f_sugar, '添加糖摄入量得分': sugar,
-                              '总热量摄入对比值': f_totalheat, '总热量摄入量得分': totalheat,
-                              '三大营养素摄入实际量': f_nutrients, '三大营养素组成得分': nutrients,
+                              '添加糖摄入实际量': f_sugar, '添加糖摄入量得分': sugar,'总热量摄入值': f_totalheat1,
+                              '总热量摄入对比值': f_totalheat2, '总热量摄入量得分': totalheat,
+                              '三大营养素碳水化合物对比值': f_nutrients1,
+                              '三大营养素脂肪对比值': f_nutrients2,
+                              '三大营养素蛋白质对比值': f_nutrients3, '三大营养素组成得分': nutrients,
                               '饮酒摄入实际量': f_alcohol, '饮酒（酒精量，全天标准）得分': alcohol,
                               '饮水量': f_water, '饮水量得分': water,'总得分':totalscore
                               })
 
 columns = ['用户编号', '姓名', '记录日期', '水果摄入实际量', '水果摄入量得分', '蔬菜摄入实际量', '蔬菜摄入量得分', '全谷类摄入实际量', '全谷类摄入量得分', '精制谷物摄入实际量',
                '精制谷物摄入量得分', '膳食纤维摄入实际量', '膳食纤维摄入量得分', '乳类摄入实际量', '乳类摄入量得分', '总蛋白摄入实际量', '总蛋白摄入量得分', '鱼虾贝壳类及植物蛋白类摄入实际量',
-               '鱼虾贝壳类及植物蛋白类摄入量得分'
-        , '不饱和与饱和脂肪酸实际摄入量', '不饱和与饱和脂肪酸摄入比得分', '固态脂肪摄入实际量', '固态脂肪摄入量得分', '钠盐摄入实际量', '钠盐摄入量得分', '添加糖摄入实际量', '添加糖摄入量得分',
-               '总热量摄入对比值', '总热量摄入量得分', '三大营养素摄入实际量', '三大营养素组成得分', '饮酒摄入实际量', '饮酒（酒精量，全天标准）得分', '饮水量', '饮水量得分','总得分']
-dataframe.to_csv('/Users/martin_yan/Desktop/babymother_data5.22-5.2812.csv', index=False, encoding="utf_8_sig", columns=columns)
-
+               '鱼虾贝壳类及植物蛋白类摄入量得分', '不饱和与饱和脂肪酸摄入对比值', '不饱和与饱和脂肪酸摄入比得分', '固态脂肪摄入实际量', '固态脂肪摄入量得分',
+                '钠盐摄入实际量', '钠盐摄入量得分', '添加糖摄入实际量', '添加糖摄入量得分','总热量摄入值',   '总热量摄入对比值', '总热量摄入量得分', '三大营养素碳水化合物对比值',
+                '三大营养素脂肪对比值','三大营养素蛋白质对比值',  '三大营养素组成得分', '饮酒摄入实际量', '饮酒（酒精量，全天标准）得分', '饮水量', '饮水量得分','总得分']
+print(dataframe)
+dataframe.to_csv('/Users/martin_yan/Desktop/bdada.csv', index=False, encoding="utf_8_sig", columns=columns)
 
 
--- BabyMother/babymother_mean.py
+++ BabyMother/babymother_mean.py
@@ -38,7 +38,9 @@ sum_sugar = 0
 sum_f_sugar = 0
 sum_totalheat = 0
 sum_nutrients = 0
-sum_f_nutrients = 0
+sum_f_nutrients1 = 0
+sum_f_nutrients2 = 0
+sum_f_nutrients3 = 0
 sum_alcohol = 0
 sum_f_alcohol = 0
 sum_water = 0
@@ -87,22 +89,24 @@ for i in range(len(data)):
     sum_sugar =  sum_sugar+data.iloc[i][27]
 
     #sum_f_totalheat = sum_f_totalheat+data.iloc[i][28]
-    sum_totalheat =  sum_totalheat+data.iloc[i][29]
+    sum_totalheat =  sum_totalheat+data.iloc[i][30]
 
-    sum_f_nutrients = sum_f_nutrients+data.iloc[i][30]
-    sum_nutrients = sum_nutrients+data.iloc[i][31]
+    sum_f_nutrients1 = sum_f_nutrients1+data.iloc[i][31]
+    sum_f_nutrients2 = sum_f_nutrients2 + data.iloc[i][32]
+    sum_f_nutrients3 = sum_f_nutrients3 + data.iloc[i][33]
+    sum_nutrients = sum_nutrients+data.iloc[i][34]
 
-    sum_f_alcohol = sum_f_alcohol+data.iloc[i][32]
-    sum_alcohol = sum_alcohol+data.iloc[i][33]
+    sum_f_alcohol = sum_f_alcohol+data.iloc[i][35]
+    sum_alcohol = sum_alcohol+data.iloc[i][36]
 
-    sum_f_water = sum_f_water+data.iloc[i][34]
-    sum_water =  sum_water+data.iloc[i][35]
+    sum_f_water = sum_f_water+data.iloc[i][37]
+    sum_water =  sum_water+data.iloc[i][38]
 
-    score = score+data.iloc[i][36]
+    score = score+data.iloc[i][39]
 
     if username[i]==False:
         num=num+1
-        """
+
         #除以实际记录天数
         dataframe = pd.DataFrame({'用户编号': [data.iloc[i][0]], '姓名': [data.iloc[i][2]], '记录天数': [count],
                                    '水果实际摄入平均量': [sum_f_fruit / count], '水果摄入量平均分': [sum_fruit / count],
@@ -118,20 +122,22 @@ for i in range(len(data)):
                                    '总蛋白摄入量平均分': [sum_totalprotein / count],
                                    '鱼虾贝壳类及植物蛋白类实际摄入平均量': [sum_f_fishshrimp / count],
                                    '鱼虾贝壳类及植物蛋白类摄入量平均分': [sum_fishshrimp / count],
-                                   '不饱和与饱和脂肪酸实际摄入平均量': [sum_f_fattyacids / count],
+                                   '不饱和与饱和脂肪酸摄入平均对比值': [sum_f_fattyacids / count],
                                    '不饱和与饱和脂肪酸摄入比平均分': [sum_fattyacids / count],
                                    '固态脂肪实际摄入平均量': [sum_f_solidfat / count],
                                    '固态脂肪摄入量平均分': [sum_solidfat / count],
                                    '钠盐实际摄入平均量': [sum_f_salt / count], '钠盐摄入量平均分': [sum_salt / count],
                                    '添加糖实际摄入平均量': [sum_f_sugar /count], '添加糖摄入量平均分': [sum_sugar / count],
                                    '总热量摄入量平均分': [sum_totalheat / count],
-                                   '三大营养素实际摄入平均量': [sum_f_nutrients / count],
+                                  '三大营养素碳水化合物平均对比值': [sum_f_nutrients1 / count],
+                                  '三大营养素脂肪平均对比值': [sum_f_nutrients2 / count],
+                                  '三大营养素蛋白质平均对比值': [sum_f_nutrients3 / count],
                                    '三大营养素组成平均分': [sum_nutrients / count],
                                    '饮酒实际摄入平均量': [sum_f_alcohol / count],
                                    '饮酒（酒精量，全天标准）平均分': [sum_alcohol / count],
                                    '饮水平均量': [sum_f_water / count], '饮水量平均分': [sum_water / count],
-                                   '平均得分': [score / count],'减重值':[data.iloc[i][37]],'初始体重值':[data.iloc[i][38]],
-                                   '减重百分比':[data.iloc[i][37]/data.iloc[i][38]],'BMI':[data.iloc[i][39]],'年龄':[data.iloc[i][40]]})
+                                   '平均得分': [score / count],'减重值':[data.iloc[i][40]],'初始体重值':[data.iloc[i][41]],
+                                   '减重百分比':[data.iloc[i][40]/data.iloc[i][41]],'BMI':[data.iloc[i][42]],'年龄':[data.iloc[i][43]]})
         """
         #除以入营以来的天数
         dataframe = pd.DataFrame({'用户编号': [data.iloc[i][0]], '姓名': [data.iloc[i][2]], '记录天数': [count],
@@ -148,30 +154,33 @@ for i in range(len(data)):
                                    '总蛋白摄入量平均分': [sum_totalprotein / 28],
                                    '鱼虾贝壳类及植物蛋白类实际摄入平均量': [sum_f_fishshrimp / 28],
                                    '鱼虾贝壳类及植物蛋白类摄入量平均分': [sum_fishshrimp / 28],
-                                   '不饱和与饱和脂肪酸实际摄入平均量': [sum_f_fattyacids / 28],
+                                   '不饱和与饱和脂肪酸摄入平均对比值': [sum_f_fattyacids / 28],
                                    '不饱和与饱和脂肪酸摄入比平均分': [sum_fattyacids / 28],
                                    '固态脂肪实际摄入平均量': [sum_f_solidfat / 28],
                                    '固态脂肪摄入量平均分': [sum_solidfat / 28],
                                    '钠盐实际摄入平均量': [sum_f_salt / 28], '钠盐摄入量平均分': [sum_salt / 28],
                                    '添加糖实际摄入平均量': [sum_f_sugar / 28], '添加糖摄入量平均分': [sum_sugar /28],
                                    '总热量摄入量平均分': [sum_totalheat / 28],
-                                   '三大营养素实际摄入平均量': [sum_f_nutrients / 28],
+                                   '三大营养素碳水化合物平均对比值': [sum_f_nutrients1 / 28],
+                                  '三大营养素脂肪平均对比值': [sum_f_nutrients2 / 28],
+                                  '三大营养素蛋白质平均对比值': [sum_f_nutrients3 / 28],
                                    '三大营养素组成平均分': [sum_nutrients / 28],
                                    '饮酒实际摄入平均量': [sum_f_alcohol / 28],
                                    '饮酒（酒精量，全天标准）平均分': [sum_alcohol / 28],
                                    '饮水平均量': [sum_f_water / 28], '饮水量平均分': [sum_water / 28],
-                                   '平均得分': [score / 28], '减重值': [data.iloc[i][37]], '初始体重值': [data.iloc[i][38]],
-                                   '减重百分比': [data.iloc[i][37] / data.iloc[i][38]], 'BMI': [data.iloc[i][39]],
-                                   '年龄': [data.iloc[i][40]]})
-
+                                   '平均得分': [score / 28], '减重值': [data.iloc[i][40]], '初始体重值': [data.iloc[i][41]],
+                                   '减重百分比': [data.iloc[i][40] / data.iloc[i][41]], 'BMI': [data.iloc[i][42]],
+                                   '年龄': [data.iloc[i][43]]})
+         """
         sec=True
         if num>1:
             sec=False
         columns = ['用户编号', '姓名', '年龄','BMI','记录天数','水果实际摄入平均量', '水果摄入量平均分', '蔬菜实际摄入平均量', '蔬菜摄入量平均分', '全谷类实际摄入平均量', '全谷类摄入量平均分',
                     '精制谷物摄入平均量', '精制谷物摄入量平均分', '膳食纤维实际摄入平均量', '膳食纤维摄入量平均分', '乳类实际摄入平均量', '乳类摄入量平均分', '总蛋白实际摄入平均量',
                     '总蛋白摄入量平均分', '鱼虾贝壳类及植物蛋白类实际摄入平均量', '鱼虾贝壳类及植物蛋白类摄入量平均分',
-                    '不饱和与饱和脂肪酸实际摄入平均量', '不饱和与饱和脂肪酸摄入比平均分', '固态脂肪实际摄入平均量', '固态脂肪摄入量平均分', '钠盐实际摄入平均量', '钠盐摄入量平均分',
-                    '添加糖实际摄入平均量', '添加糖摄入量平均分', '总热量摄入量平均分', '三大营养素实际摄入平均量', '三大营养素组成平均分', '饮酒实际摄入平均量',
+                    '不饱和与饱和脂肪酸摄入平均对比值', '不饱和与饱和脂肪酸摄入比平均分', '固态脂肪实际摄入平均量', '固态脂肪摄入量平均分', '钠盐实际摄入平均量',
+                   '钠盐摄入量平均分','添加糖实际摄入平均量', '添加糖摄入量平均分', '总热量摄入量平均分',   '三大营养素碳水化合物平均对比值','三大营养素脂肪平均对比值',
+                    '三大营养素蛋白质平均对比值', '三大营养素组成平均分', '饮酒实际摄入平均量',
                     '饮酒（酒精量，全天标准）平均分', '饮水平均量', '饮水量平均分','平均得分','初始体重值','减重值','减重百分比']
 
         dataframe.to_csv('/Users/martin_yan/Desktop/2.csv', index=False, encoding="utf_8_sig",
@@ -203,7 +212,9 @@ for i in range(len(data)):
         sum_f_sugar = 0
         sum_totalheat = 0
         sum_nutrients = 0
-        sum_f_nutrients = 0
+        sum_f_nutrients1 = 0
+        sum_f_nutrients2 = 0
+        sum_f_nutrients3 = 0
         sum_alcohol = 0
         sum_f_alcohol = 0
         sum_water = 0
@@ -212,7 +223,7 @@ for i in range(len(data)):
         count = 0
 
 
-data=pd.read_csv('/Users/martin_yan/Desktop/每日热量统计.csv',usecols=[1,5,6])
+data=pd.read_csv('/Users/martin_yan/Desktop/每日热量统计.csv',usecols=[1,4,5,6])
 light=pd.read_csv('../data/轻食日统计.csv')
 data = pd.merge(data, light, on='姓名')
 print(len(data['姓名'].unique()))
@@ -233,49 +244,71 @@ for i in range(len(data)):
 data['是否为轻食日']=lightday
 first=data.duplicated('姓名',keep='first')
 last=data.duplicated('姓名',keep='last')
+
+sum_f_normal=0
+sum_f_light=0
 sum_normal=0
 sum_light=0
+
 countlight = 0
 countnormal = 0
+
+meanflight=[]
+meanfnormal=[]
 meanlight=[]
 meannormal=[]
+
 name=[]
 print(data)
 for i in range(len(data)):
+    value = data.iloc[i]['摄入值']
     number = data.iloc[i]['对比值']
+
     if data.iloc[i]['是否为轻食日'] == 1:
+        sum_f_light  = sum_f_light + value
         sum_light = sum_light + number
         countlight=countlight+1
 
     else:
+        sum_f_normal = sum_f_normal + value
         sum_normal = sum_normal + number
         countnormal=countnormal+1
 
     if last[i] == False:
         name.append(data.iloc[i]['姓名'])
 
-        """
+
         #除以记录天数
         if countlight==0:
             meanlight.append(0)
+            meanflight.append(0)
         else:
             meanlight.append(sum_light/countlight)
+            meanflight.append(sum_f_light/countlight)
         if countnormal==0:
             meannormal.append(0)
+            meanfnormal.append(0)
         else:
             meannormal.append(sum_normal/countnormal)
+            meanfnormal.append(sum_f_normal/countnormal)
+
         """
 
         #除以实际入营天数
+        meanflight.append(sum_f_light / 8)
         meanlight.append(sum_light / 8)
+        meanfnormal.append(sum_f_normal / 20)
         meannormal.append(sum_normal / 20)
+        """
 
         sum_light=0
         sum_normal=0
+        sum_f_light = 0
+        sum_f_normal = 0
         countlight=0
         countnormal=0
 
-meanheat=pd.DataFrame({'姓名':name,'轻食日热量平均对比值':meanlight,'普通日热量平均对比值':meannormal})
+meanheat=pd.DataFrame({'姓名':name,'轻食日热量平均摄入值':meanflight,'普通日热量平均摄入值':meanfnormal,'轻食日热量平均对比值':meanlight,'普通日热量平均对比值':meannormal})
 mean=pd.read_csv('/Users/martin_yan/Desktop/2.csv')
 data = pd.merge(mean, meanheat, on='姓名')
 data.to_csv('/Users/martin_yan/Desktop/3.csv', index=False, encoding="utf_8_sig")
