d5c3ab8502279e0d53c2bdc15e8a6671d5148fd3
Martin_Yan <Maitong_y@163.com>
2018-07-04 13:51:24 +0800

    add random forest, svm method to predict the weight.

--- BabyMother/prediction.py
+++ BabyMother/prediction.py
@@ -1,31 +1,52 @@
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
-from sklearn.decomposition import PCA
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
-from sklearn.ensemble import RandomForestClassifier,GradientBoostingRegressor,AdaBoostClassifier
-from sklearn.svm import SVR
 from sklearn.kernel_ridge import KernelRidge
 from xgboost import XGBRegressor
+from sklearn import svm
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
 
 
 train=pd.read_csv('/Users/martin_yan/Desktop/mean_babymother_data5.22-6.25(体重跨度5周／实际记录平均分).csv')
-
+plt.rcParams['font.sans-serif']=[u'SimHei']
+plt.rcParams['axes.unicode_minus']=False
 #选取部分属性作为预测标准
-predictors=['膳食纤维摄入量平均分','蔬菜摄入量平均分','平均得分','添加糖摄入量平均分','固态脂肪摄入量平均分','精制谷物摄入量平均分',
-'总蛋白摄入量平均分','记录天数','不饱和与饱和脂肪酸摄入平均对比值']
+predictors=['记录天数','平均得分','完整记录天数','膳食纤维摄入量平均分','蔬菜摄入量平均分','固态脂肪摄入量平均分','水果摄入量平均分',
+'全谷类实际摄入平均量','精制谷物摄入量平均分','水果实际摄入平均量','全谷类摄入量平均分','总热量摄入量平均分','鱼虾贝壳类及植物蛋白类摄入量平均分',
+'饮水量平均分','不饱和与饱和脂肪酸摄入平均对比值','不饱和与饱和脂肪酸摄入比平均分','饮水平均量','初始体重值','BMI','三大营养素蛋白质平均对比值',
+'膳食纤维实际摄入平均量','钠盐实际摄入平均量','总蛋白摄入量平均分','普通日热量平均摄入值','乳类实际摄入平均量','乳类摄入量平均分','三大营养素碳水化合物平均对比值',
+'三大营养素组成平均分','蔬菜实际摄入平均量','鱼虾贝壳类及植物蛋白类实际摄入平均量','轻食日热量平均对比值','总蛋白实际摄入平均量','轻食日热量平均摄入值',
+'添加糖摄入量平均分','普通日热量平均对比值','年龄','钠盐摄入量平均分','饮酒实际摄入平均量','添加糖实际摄入平均量','三大营养素脂肪平均对比值',
+'精制谷物摄入平均量','固态脂肪实际摄入平均量','饮酒（酒精量，全天标准）平均分']
 
 #找出存在nan的行号
 #print(np.where(np.isnan(train['体重']))[0])
 #根据行号去删除某些行
 #train.drop([2,12] ,axis=0, inplace=True)
 
+
 target=train['减重值']
-x_train, x_test, y_train, y_test = train_test_split(train[predictors], target, test_size=.1)
-lr=linear_model.LinearRegression()
+x_train, x_test, y_train, y_test = train_test_split(train[predictors], target,random_state=13, test_size=.1)
+algorithms=[
+RandomForestRegressor(n_estimators =300,criterion='mse',min_samples_leaf=6,max_depth=3,random_state=1,n_jobs=-1),
+#svm.SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma='auto',kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+]
+full_predictions = []
+for lr in algorithms:
+    model = lr.fit(x_train, y_train)
+    predictions=model.predict(x_test)
+    full_predictions.append(predictions)
+predictions = full_predictions[0]
+print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+
+
+
+"""
+#lr=linear_model.LinearRegression()
 #lr=linear_model.RidgeCV(alphas=np.logspace(-3, 2, 100)),
 #lr=XGBRegressor(max_depth=5)
 model = lr.fit(x_train, y_train)
@@ -34,3 +55,60 @@ print(predictions)
 print(y_test)
 print ('RMSE is: \n', mean_squared_error(y_test, predictions))
 
+
+clf = svm.SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma='auto',kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+model=clf.fit(x_train, y_train)
+prediction=model.predict(x_test)
+print(prediction)
+print(y_test)
+print ('RMSE is: \n', mean_squared_error(y_test, prediction))
+
+
+rmse=[]
+pa=[]
+for i in range(1,10):
+    forest = RandomForestRegressor(n_estimators =300,criterion='mse',min_samples_leaf=6,max_depth=3,random_state=1,n_jobs=-1)
+    model=forest.fit(x_train, y_train)
+    prediction = model.predict(x_test)
+    print('第'+str(i)+'次尝试')
+    #print(prediction)
+    #print(y_test)
+    #print ('RMSE is: \n', mean_squared_error(y_test, prediction))
+    rmse.append(mean_squared_error(y_test, prediction))
+    pa.append(i)
+
+plt.title('rmse随参数n的变化图')
+plt.xlabel('n')
+plt.ylabel('rmse')
+plt.plot(pa,rmse,'r', label='broadcast')
+plt.show()
+
+for i in range(0,50):
+    target=train['减重值']
+    x_train, x_test, y_train, y_test = train_test_split(train[predictors], target,random_state=i, test_size=.1)
+
+    algorithms=[
+    RandomForestRegressor(n_estimators =300,criterion='mse',min_samples_leaf=6,max_depth=3,random_state=1,n_jobs=-1),
+    #svm.SVR(C=1.0, cache_size=200, coef0=0.0, degree=3, epsilon=0.1, gamma='auto',kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
+    linear_model.LinearRegression()
+    ]
+
+    full_predictions = []
+    best=100
+    parameter=0
+    for j in range(0,100):
+        for lr in algorithms:
+            a=j*0.01
+            model = lr.fit(x_train, y_train)
+            predictions=model.predict(x_test)
+            full_predictions.append(predictions)
+        predictions = (full_predictions[0]*a + full_predictions[1]*(1-a))
+        print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+        rmse=mean_squared_error(y_test, predictions)
+        if rmse<best:
+            best=rmse
+            parameter=a
+
+    print('最小的rmse值:'+str(best)+',最好的权重a:'+str(parameter))
+
+"""
--- WheatGreen/seekweight.py
+++ WheatGreen/seekweight.py
@@ -1,30 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-data = pd.read_csv('/Users/martin_yan/Desktop/HEI_mean_babymother_data5.22-6.25(总表／实际记录得分).csv')
-#target = np.log(data['减重值'])
-#print ("Skew is:", target.skew())
-#plt.hist(target, color='blue')
-#plt.show()
+data = pd.read_csv('/Users/martin_yan/Desktop/HEI_mean_babymother_data5.22-6.25(总表／入营得分).csv')
+data2=pd.read_csv('/Users/martin_yan/Desktop/新热量区间换算热量分值.csv',usecols=['姓名','轻食日热量平均分(入营天数)','普通日热量平均分(入营天数)'])
+data = pd.merge(data, data2, on='姓名')
+
 balanced=[]
 totalheat=[]
+light=[]
+normal=[]
 for i in range(len(data)):
     #score=data.iloc[i]['平均得分']
-    totalheat.append(data.iloc[i]['总热量摄入量平均分'])
+    #totalheat.append(data.iloc[i]['总热量摄入量平均分'])
     #balanced.append((score-totalheat[i])/11)
+    light.append(data.iloc[i]['轻食日热量平均分(入营天数)'])
+    normal.append(data.iloc[i]['普通日热量平均分(入营天数)'])
     balanced.append(data.iloc[i]['饮食均衡得分'] / 12)
 
-#print(balanced)
 best=0
 parameter=0
-for i in range(0,1000):
-    a=i*0.001
-    print(a)
-    for j in range(len(data)):
-        data.loc[j,'新得分']=float(balanced[j]*a+totalheat[j]*(1-a))
-    now=data['减重值'].corr(data['新得分'])
-    print(now)
-    if now>best:
-        best=now
-        parameter=a
-print('最好的相关性系数是:' + str(best)+', 饮食均衡分数权重是:'+ str(parameter))
+for i in range(0,100):
+    for j in range(0, 100):
+        a=i*0.01
+        b=j*0.01
+        print(a)
+        print(b)
+        for num in range(len(data)):
+            data.loc[num,'新得分']=float(light[num]*a+normal[num]*b+balanced[num]*(1-a-b))
+        now=data['减重值'].corr(data['新得分'])
+        print(now)
+        if now>best:
+            best=now
+            parameter1=a
+            parameter2 =b
+print('最好的相关性系数是:' + str(best)+', 轻食日分数权重是:'+ str(parameter1)+', 轻食日分数权重是:'+ str(parameter2))
